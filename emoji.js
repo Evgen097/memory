dcc2f25336d43307b4f86e0d884034d5d231f700
Evgen097 <k0990245853@gmail.com>
2018-10-21 10:37:25 +0300

    game mechanics added

---
+++ emoji.js
@@ -0,0 +1,88 @@
+
+class Emoji {
+    constructor(parent, index, simbol){
+        this.parent = parent;
+        this.emoji = this.createElement(index, simbol);
+        this.index = index;
+    }
+
+
+    createElement(index, symbol){
+        var elem = document.createElement('div');
+        elem.className = "emoji_" + index;
+        elem.dataset.symbol = symbol;
+
+        var card = document.createElement('div')
+        card.className = "card card_" + index;
+
+        var card_face_front = document.createElement('div');
+        card_face_front.className = "card__face card__face--front";
+        card_face_front.textContent =  symbol;
+
+        var card_face_back = document.createElement('div');
+        card_face_back.className = "card__face card__face--back";
+        card_face_back.textContent =  symbol;
+
+        card.appendChild(card_face_front);
+        card.appendChild(card_face_back);
+
+        elem.appendChild(card);
+
+        return elem;
+    }
+
+    appendToParent(){
+        this.parent.appendChild(this.emoji);
+    }
+
+    clickEvent(){
+        // console.log(this)
+        this.emoji.childNodes[0].classList.toggle('is-flipped');
+        return this;
+    }
+    addColor(color){
+        switch ( color ) {
+            case 'green':
+                this.emoji.childNodes[0].childNodes[1].style.backgroundColor  = '#4cff4d';
+                break;
+            case 'red':
+                this.emoji.childNodes[0].childNodes[1].style.backgroundColor  = '#ff474d';
+                break;
+            case 'white':
+                this.emoji.childNodes[0].childNodes[1].style.backgroundColor  = '#fff';
+                break;
+            default:
+                console.log('color didnt match');
+        }
+    }
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- script.js
+++ script.js
@@ -4,19 +4,90 @@ window.onload = function() {
         constructor(container){
             this.emojisContainer = document.querySelector(container);
             this.symbols = ['🐶', '🐱', '🐭', '🐹', '🐰', '🐻', '🐼', '🐨', '🐯', '🦁', '🐮', '🐷', '🐸', '🐙', '🐵', '🦄', '🐞', '🦀', '🐟', '🐊', '🐓', '🦃'];
+            this.emojis = [];
+            this.openedEmojis = [];
+            this.openedEmojisIndex = [];
+            this.matchedEmojisIndex = [];
         }
 
         init(){
-            this.emojisContainer.addEventListener( 'click', this.clickOnEmoji);
+            this.emojisContainer.addEventListener( 'click', this.clickOnEmoji.bind(this));
             this.createEmojis();
+            this.appendEmojisToDom()
         }
 
 
         clickOnEmoji(event){
-            let parent = event.target.parentElement;
-            if(parent.classList.contains('card') ){
-                parent.classList.toggle('is-flipped')
+
+            var emojiClass = event.target.parentElement.parentElement.classList[0];
+            var valid = /emoji_/.test( emojiClass )
+            if (!valid) return;
+            var index = Number( emojiClass.replace( /emoji_/, '') );
+
+            var emoji = this.emojis.filter( emoji => index === emoji.index)[0];
+
+            this.handleClick(emoji);
+        }
+
+        handleClick(emoji){
+
+            switch ( this.openedEmojisIndex.length ) {
+                case 0:
+                    console.log('case 0')
+                    if( this.matchedEmojisIndex.indexOf(emoji.index) >= 0  ) return;
+                    emoji.clickEvent();
+                    if ( this.openedEmojisIndex.indexOf(emoji.index) < 0 ) this.openedEmojisIndex.push(emoji.index);
+                    break;
+
+                case 1:
+                    console.log('case 1')
+                    if( this.matchedEmojisIndex.indexOf(emoji.index) >= 0  ) return;
+                    if( this.openedEmojisIndex.indexOf(emoji.index) >= 0  ) {
+                        emoji.clickEvent();
+                        this.openedEmojisIndex = [];
+                        return
+                    };
+                    emoji.clickEvent();
+
+                    var previosEmoji = this.emojis[ this.openedEmojisIndex[0] ];
+                    var match = this.compareEmojiBySymbol(previosEmoji, emoji);
+                    if(match){
+                        console.log('match')
+                        previosEmoji.addColor('green');
+                        emoji.addColor('green');
+                        this.matchedEmojisIndex.push(previosEmoji.index)
+                        this.matchedEmojisIndex.push(emoji.index)
+                        this.openedEmojisIndex = [];
+
+                    }else {
+                        console.log('not match')
+                        previosEmoji.addColor('red');
+                        emoji.addColor('red');
+                        this.openedEmojisIndex.push(emoji.index)
+                    }
+                    break;
+                case 2:
+                    console.log('case 2')
+                    if( this.matchedEmojisIndex.indexOf(emoji.index) >= 0  ) return;
+                    if( this.openedEmojisIndex.indexOf(emoji.index) >= 0  ) return;
+
+                    emoji.clickEvent();
+
+                    this.emojis[ this.openedEmojisIndex[0] ].clickEvent().addColor('white');
+                    this.emojis[ this.openedEmojisIndex[1] ].clickEvent().addColor('white');
+
+                    this.openedEmojisIndex = [];
+                    this.openedEmojisIndex.push(emoji.index);
+                    break;
+                default:
+                    console.log('hello default')
             }
+
+        }
+
+        compareEmojiBySymbol(elem_1, elem_2){
+            return  elem_1.emoji.dataset.symbol == elem_2.emoji.dataset.symbol ;
+
         }
 
         mixedArr(arr) {
@@ -34,35 +105,28 @@ window.onload = function() {
 
 
         createEmojis() {
-            let mixedSymbols = this.mixedArr(this.symbols).splice(0, 6)
-            let emojiSimbols = this.mixedArr(mixedSymbols.concat(mixedSymbols))
+            let emojiSimbols = this.genereteEmojiSet();
             for (var i=0; i < 12; i++){
-                this.createEmojiElement(i, emojiSimbols[i])
+                var newEmoji = new Emoji(this.emojisContainer, i, emojiSimbols[i]);
+                this.emojis.push(newEmoji);
             }
         }
 
-        createEmojiElement(index, simbol){
-            var emoji = document.createElement('div');
-            emoji.className = "emoji_" + index;
-
-            var card = document.createElement('div')
-            card.className = "card card_" + index;
-
-            var card_face_front = document.createElement('div');
-            card_face_front.className = "card__face card__face--front";
-
-
-            var card_face_back = document.createElement('div');
-            card_face_back.className = "card__face card__face--back";
-            card_face_back.textContent =  simbol;
-
-            card.appendChild(card_face_front);
-            card.appendChild(card_face_back);
-
-            emoji.appendChild(card);
-
-            this.emojisContainer.appendChild(emoji)
+        genereteEmojiSet(){
+            let mixedSymbols = this.mixedArr(this.symbols).splice(0, 6)
+            let twelveSimbols = this.mixedArr(mixedSymbols.concat(mixedSymbols));
+            return twelveSimbols;
         }
+
+
+        appendEmojisToDom(){
+            this.emojis.forEach( emoji => emoji.appendToParent())
+        }
+
+
+
+
+
     }
 
 
@@ -82,7 +146,7 @@ window.onload = function() {
 // echo "# memory" >> README.md
 // git init
 // git add README.md
-// git commit -m "first commit"
+// git commit -m "game mechanics added"
 // git remote add origin https://github.com/Evgen097/memory.git
 //     git push -u origin master
 
@@ -103,7 +167,10 @@ window.onload = function() {
 
 
 
-
+// let parent = event.target.parentElement;
+// if(parent.classList.contains('card') ){
+//     parent.classList.toggle('is-flipped')
+// }
 
 
 
